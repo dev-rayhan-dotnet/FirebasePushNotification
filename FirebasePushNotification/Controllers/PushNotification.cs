20537e470de396738a434b82f0f956f301217577
rayhancse1102029 <rayhan.cse15@gmail.com>
2024-10-01 13:46:26 +0600

    process

--- FirebasePushNotification/Controllers/PushNotification.cs
+++ FirebasePushNotification/Controllers/PushNotification.cs
@@ -12,6 +12,9 @@ namespace FirebasePushNotification.Controllers
         [HttpPost]
         public async Task<IActionResult> SendPushNotification(PushMessagePayload? request)
         {
+            // https://cedricgabrang.medium.com/firebase-cloud-messaging-with-asp-net-core-df666291c427
+            // https://firebase.google.com/docs/cloud-messaging/send-message#c
+            // https://github.com/andrei-m-code/net-core-push-notifications
             // device id o ABU Rayhan vivo v20 ### 317dd17953b9a5cb ###
 
             request = new PushMessagePayload("Push Notify", "Test Push Notification", "cOOJeXG8TXGOz8yDthX2xx:APA91bEO1QtvqDjm5IYbKKR2r-BVCiWSz6J8ncN4ldMLe4ELF18WfelBCFnvN2v9b_yLBzwFF-GSVSRqasmAmhaDHJwAuYUyt0XA1e2_o3KUMvJ0g_pehabnjYQKgHgaMYMuu1m73mND");
